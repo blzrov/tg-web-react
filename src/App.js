fa7aa00e64f572bf6b7396e8eb50a3ae4ccfaae7
Denis Belozerov <molotov.ant@mail.ru>
2022-10-11 16:46:26 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  React.useEffect(() => tg.ready(), []);
-
-  
   return (
     <div className="App">
-      <button onClick={onClose}>Close</button>
+      <Header />
     </div>
   );
 }
