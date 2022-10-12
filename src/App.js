a2c7af833cea5b6ecc4fb248d0abc7595507c249
Denis Belozerov <molotov.ant@mail.ru>
2022-10-12 12:41:01 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import useTelegram from "../../hooks/useTelegram";
 import Header from "./components/Header/Header";
 
 function App() {
+  const { onToggleButton, tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
   return (
     <div className="App">
+      <button onClick={onToggleButton}>toggle</button>
       <Header />
     </div>
   );
